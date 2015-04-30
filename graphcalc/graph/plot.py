0fc3bd164d78827ce3207a595146bdabe537785e
Jacob Adams <tookmund@gmail.com>
2015-04-30 18:26:25 -0400

    Parse math equations with py_eval_expressions

--- graphcalc/graph/plot.py
+++ graphcalc/graph/plot.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
+from py_expression_eval import Parser
 
-def yeq(equation,range):
+parser = Parser()
+def yeq(equation,rg):
 	xlist = []
 	ylist = []
-	for x in range:
+	for x in rg:
 		xlist.append(x)
-		y = eval(equation)
+		y = parser.evaluate(equation, {'x': x})
 		ylist.append(y)
-	
+		print("x: %s y: %s" %(x,y))
 	plt.plot(xlist,ylist)
 	plt.show()
