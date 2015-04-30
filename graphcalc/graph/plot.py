d85e108f410f9d71b397e122e0d03e1c470dfbfe
Jacob Adams <tookmund@gmail.com>
2015-04-30 18:30:30 -0400

    Add xeq

--- graphcalc/graph/plot.py
+++ graphcalc/graph/plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from py_expression_eval import Parser
 
 parser = Parser()
+
 def yeq(equation,rg):
 	xlist = []
 	ylist = []
@@ -9,6 +10,17 @@ def yeq(equation,rg):
 		xlist.append(x)
 		y = parser.evaluate(equation, {'x': x})
 		ylist.append(y)
-		print("x: %s y: %s" %(x,y))
+		#print("x: %s y: %s" %(x,y))
+	plt.plot(xlist,ylist)
+	plt.show()
+
+def xeq(equation,rg):
+	xlist = []
+	ylist = []
+	for y in rg:
+		ylist.append(y)
+		x = parser.evaluate(equation, {'y': y})
+		xlist.append(x)
+		#print("x: %s y: %s" %(x,y))
 	plt.plot(xlist,ylist)
 	plt.show()
