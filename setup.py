dcd821cfe16c0d5d0c90db922b3edaf1f6cc4244
Jacob Adams <tookmund@gmail.com>
2015-05-01 10:30:28 -0400

    Setup setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+import sys
+from cx_Freeze import setup, Executable
+build_exe_options = {"packages": ["os","matplotlib","tkinter"]}
+setup( name = "GraphCalc",
+	version = "0.1",
+	description = "Graphing Calculator in Python",
+	options = {"build_exe": build_exe_options},
+	executables = [Executable("run.py",base=None)])
