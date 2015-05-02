6ffe0e5e33688ee7e63f060658c162233302659e
Jacob Adams <tookmund@gmail.com>
2015-05-02 16:48:56 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from cx_Freeze import setup, Executable
 build_exe_options = {"packages": ["os","matplotlib","tkinter"]}
 setup( name = "GraphCalc",
-	version = "0.1",
+	version = "0.2",
 	description = "Graphing Calculator in Python",
 	options = {"build_exe": build_exe_options},
 	executables = [Executable("GraphCalc.py",base=None)])
