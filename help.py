2715c1d0ed05d0877af071b27b664ecce068e3a8
Jacob Adams <tookmund@gmail.com>
2015-05-02 10:43:03 -0400

    Fix up help

--- help.py
+++ help.py
@@ -1,19 +1,20 @@
 help = {
 '/help' : 'Show this menu',
-'/set (name) (value)' : 'Set variable',
-'/get (name)' : 'Get variable',
-'/graph (y/x) (equation) (min) (max)' : 'Graph a function', 
-'/stats (list)' : 'Calculate statistics',
-'/perm (total) (pick)' : 'Calculate permuations',
-'/combo (total) (pick)' : 'Calulate combinations'
+'/set' : '(name) (value) Set variable',
+'/get' : '(name) Get variable',
+'/graph' : '(y/x) (equation) (min) (max) Graph a function', 
+'/stats' : '(list) Calculate statistics',
+'/perm' : '(total) (pick) Calculate permuations',
+'/combo' : '(total) (pick) Calulate combinations'
 }
 def helper(str):
-	if str[1]:
+	try:
 		com = '/'+str[1]
-		return(help[com])
-	else:
+		print("%s : %s" % (com,help[com]))
+	except:
 		print("""
 Type any expression and it will simplify it to a single number
 Ctrl+C to exit
-"")
-		return help
+""")
+		for x,y in help.items():
+			print("%s : %s" % (x,y))
