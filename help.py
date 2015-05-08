e941b7231900ae50557861664378018dc8a7f841
Jacob Adams <tookmund@gmail.com>
2015-05-08 15:08:27 -0400

    Cleanup help

--- help.py
+++ help.py
@@ -1,25 +1,26 @@
 help = {
-'/help' : 'Show this menu',
-'/set' : '(name) (value) Set variable',
-'/get' : '(name) Get variable',
-'/graph' : '(y/x) (equation) (min) (max) Graph a function', 
-'/stats' : '(list) Calculate statistics',
-'/perm' : '(total) (pick) Calculate permuations',
-'/combo' : '(total) (pick) Calulate combinations'
+'/help' : '(name): Show usage of command. Without arguments, show all',
+'/set' : '(name) (value): Set variable',
+'/get' : '(name): Get variable',
+'/graph' : '(y/x) (equation) (min) (max): Graph a function', 
+'/stats' : '(list): Calculate statistics',
+'/perm' : '(total) (pick): Calculate permuations',
+'/combo' : '(total) (pick): Calulate combinations',
+'/sigma' : '(equation) (variable) (start) (end): Calcuate summation'
 }
 def helper(st):
 	if isinstance(st,str):
 		com = '/'+st
 		print("""Wrong Arguments: 
-%s : %s""" % (com,help[com]))
+%s %s""" % (com,help[com]))
 		return None
 	try:
 		com = '/'+st[1]
-		print("%s : %s" % (com,help[com]))
+		print("%s %s" % (com,help[com]))
 	except:
 		print("""
 Type any expression and it will simplify it to a single number
 Ctrl+C to exit
 """)
 		for x,y in help.items():
-			print("%s : %s" % (x,y))
+			print("%s %s" % (x,y))
