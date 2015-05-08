2777dc2da23735a43553e14670e09f78d67e2813
Jacob Adams <tookmund@gmail.com>
2015-05-08 16:21:39 -0400

    Clean up help again

--- src/help.py
+++ src/help.py
@@ -1,12 +1,12 @@
 help = {
-'/help' : '(name): Show usage of command. Without arguments, show all',
-'/set' : '(name) (value): Set variable',
-'/get' : '(name): Get variable',
-'/graph' : '(y/x) (equation) (min) (max): Graph a function', 
-'/stats' : '(list): Calculate statistics',
-'/perm' : '(total) (pick): Calculate permuations',
-'/combo' : '(total) (pick): Calulate combinations',
-'/sigma' : '(equation) (variable) (start) (end): Calcuate summation'
+'/help' : '[name]: Show usage of command. Without arguments, show all',
+'/set' : '<name> <value>: Set variable',
+'/get' : '<name>: Get variable',
+'/graph' : '<y/x> <equation> <min> <max> [figure]: Graph a function. If figure given, graph in the given figure', 
+'/stats' : '<list>: Calculate statistics',
+'/perm' : '<total> <pick>: Calculate permuations',
+'/combo' : '<total> <pick>: Calulate combinations',
+'/sigma' : '<equation> <variable> <start> <end>: Calcuate summation'
 }
 def helper(st):
 	if isinstance(st,str):
@@ -23,4 +23,4 @@ Type any expression and it will simplify it to a single number
 Ctrl+C to exit
 """)
 		for x,y in help.items():
-			print("%s %s" % (x,y))
+			print("%s %s\n" % (x,y))
