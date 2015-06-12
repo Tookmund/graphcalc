ec17cd74cf97bac192ef800c36b72dc948978db6
Jacob Adams <tookmund@gmail.com>
2015-06-12 17:50:33 -0400

    Rewrite intersect using scipy and stackoverflow

--- src/intersection.py
+++ src/intersection.py
@@ -1,21 +1,42 @@
 from graph import genpoints
 import help
+import scipy.interpolate as interpolate
+import scipy.optimize as optimize
+import numpy as np
 
 helper = {'/intersect':'<equation1> <equation2> <min> <max>: Calculate intersection between two y= equations'}
 coms = {'/intersect':intersect}
+
+def pdiff(x):
+    return p1(x)-p2(x)
+
 def intersect(args):
     if len(args) < 4:
         help.helper("intersect")
         return None
     eq1 = args[1]
     eq2 = args[2]
-    rg = range(int(float(args[3])),int(float(args[4])))
-    
+    rg = range(int(float(args[3])),int(float(args[4]))) 
     pts1x,pts1y = genpoints(eq1,rg,'x')
     pts2x,pts2y = genpoints(eq2,rg,'x')
-    out = []
-    for x in rg:
-        if pts1x[x] == pts2x[x] and pts1y[x] == pts2y[x]:
-            out.append((pts1x[x],pts1y[x]))
-    for x in out:
-        print("%s\n" % (x,))
+    # http://stackoverflow.com/a/8095198
+    x1=np.array(pts1x)
+    y1=np.array(pts1y)
+    x2=np.array(pts2x)
+    y2=np.array(pts2y)    
+    p1=interpolate.PiecewisePolynomial(x1,y1[:,np.newaxis])
+    p2=interpolate.PiecewisePolynomial(x2,y2[:,np.newaxis])
+    xs=np.r_[x1,x2]
+    xs.sort()
+    x_min=xs.min()
+    x_max=xs.max()
+    x_mid=xs[:-1]+np.diff(xs)/2
+    roots=set()
+    for val in x_mid:
+        root,infodict,ier,mesg = optimize.fsolve(pdiff,val,full_output=True)
+        # ier==1 indicates a root has been found
+        if ier==1 and x_min<root<x_max:
+            roots.add(root[0])
+    roots=list(roots)
+    for i in roots:
+        print("(%d,%d)" % roots[i],p1(roots))
