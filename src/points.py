cf0488f1d2e8eb83d2d7ced0d18dc1463d6f4ecc
Jacob Adams <tookmund@gmail.com>
2015-06-24 18:13:35 -0400

    fix point crash

--- src/points.py
+++ src/points.py
@@ -10,8 +10,9 @@ def plot(args):
 	ylist = []
 	for x in args[1:]:
 		pt = x.split(",")
-		xlist.append(pt[0])
-		ylist.append(pt[1])
+		if len(pt) > 1:
+			xlist.append(pt[0])
+			ylist.append(pt[1])
 	plt.plot(xlist,ylist,'o')
 
 coms = {'/points': plot}
