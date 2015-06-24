6231fcb3525d1d3ce705328acf89365fde0bfb45
Jacob Adams <tookmund@gmail.com>
2015-06-24 18:24:58 -0400

    add numpy dep

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import sys
 from cx_Freeze import setup, Executable
-build_exe_options = {"packages": ["os","matplotlib","tkinter"]}
+build_exe_options = {"packages": ["os","matplotlib","tkinter","numpy"]}
 setup( name = "GraphCalc",
 	version = "0.3",
 	description = "Graphing Calculator in Python",
